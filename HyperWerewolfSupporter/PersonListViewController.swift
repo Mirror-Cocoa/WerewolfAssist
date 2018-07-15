a09455668e01ea0547d3c3e91d4ef5c21468ae0e
Ichiro Miura <great_faily@icloud.com>
2018-07-15 11:38:12 +0900

    人物追加、編集機能の実装。削除処理途中まで

--- HyperWerewolfSupporter/PersonListViewController.swift
+++ HyperWerewolfSupporter/PersonListViewController.swift
@@ -23,12 +23,15 @@ class PersonListViewController: UIViewController, UITableViewDelegate, UITableVi
         self.navigationItem.title = "人物登録"
         
         // ナビゲーションバーの右側に編集ボタンを追加.
+        self.editButtonItem.title = "削除"
         self.navigationItem.rightBarButtonItem = self.editButtonItem
+    
+
         
     }
     
-    //MARK: - UITableView Delegate Method
     
+    //MARK: - UITableView Delegate Method
     /*
      Cellが選択された際に呼び出される.
      */
@@ -41,13 +44,10 @@ class PersonListViewController: UIViewController, UITableViewDelegate, UITableVi
         // ダイアログを出して、名前を更新。一番下なら追加。
         if indexPath.row != myItems.count - 1 {
             // 編集モード
-            allocateMode(editType: .editMode, personName: myItems[indexPath.row] as! String)
-            
+            allocateMode(editType: .editMode, personName: myItems[indexPath.row] as! String, currentRow: indexPath.row)
         } else {
             // 追加モード
-            allocateMode(editType: .addMode, personName: myItems[indexPath.row] as! String)
-            
-            
+            allocateMode(editType: .addMode, personName: myItems[indexPath.row] as! String, currentRow: indexPath.row)
         }
         
         // TableViewを再読み込み
@@ -83,10 +83,12 @@ class PersonListViewController: UIViewController, UITableViewDelegate, UITableVi
         // 編集中のときのみaddButtonをナビゲーションバーの左に表示する
         if editing {
             print("編集中")
+            self.editButtonItem.title = "完了"
             let addButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.add, target: self, action: #selector(self.addCell(sender:)))
             self.navigationItem.setLeftBarButton(addButton, animated: true)
         } else {
             print("通常モード")
+            self.editButtonItem.title = "削除"
             self.navigationItem.setLeftBarButton(nil, animated: true)
         }
     }
@@ -95,11 +97,8 @@ class PersonListViewController: UIViewController, UITableViewDelegate, UITableVi
      addButtonが押された際呼び出される
      */
     @objc func addCell(sender: AnyObject) {
-        print("追加")
         // myItemsに追加.
         myItems.add("add Cell")
-        // TableViewを再読み込み.
-        personTableView.reloadData()
     }
 
     /*
@@ -107,16 +106,24 @@ class PersonListViewController: UIViewController, UITableViewDelegate, UITableVi
      */
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         
+//        let deleteButton: UITableViewRowAction = UITableViewRowAction(style: .normal, title: "削除") { (action, index) -> Void in
+//            self.myItems.remove(indexPath.row)
+//            tableView.deleteRows(at: [indexPath], with: .fade)
+//
+//            self.personTableView.reloadData()
+//        }
         // 削除のとき.
         if editingStyle == UITableViewCellEditingStyle.delete {
             print("削除")
-            
             // 指定されたセルのオブジェクトをmyItemsから削除する.
             myItems.removeObject(at: indexPath.row)
             
             // TableViewを再読み込み.
             personTableView.reloadData()
         }
+        
+        
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -125,68 +132,68 @@ class PersonListViewController: UIViewController, UITableViewDelegate, UITableVi
     
     
     //MARK: - Private Method
-    func allocateMode(editType: Mode, personName: String) {
+    func allocateMode(editType: Mode, personName: String, currentRow: Int) {
         var alertTitle:String = ""
         var alertMsg:String = ""
         
+        // 追加or編集？
         switch editType {
-        case .addMode:
-            alertTitle = "人物の登録を行います。"
-            alertMsg = "登録したい人の名前を入力してください。"
-            break
         case .editMode:
             alertTitle = "人物の編集を行います。"
             alertMsg = "編集したい人の名前を編集してください。"
             break
-        default:
-            
+        case .addMode:
+            alertTitle = "人物の登録を行います。"
+            alertMsg = "登録したい人の名前を入力してください。"
             break
         }
         
+        // アラートの初期設定
         let alert: UIAlertController = UIAlertController(title: alertTitle, message: alertMsg, preferredStyle:  UIAlertControllerStyle.alert)
         
+        // OKボタンの処理
         let defaultAction: UIAlertAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler:{
             // ボタンが押された時の処理を書く（クロージャ実装）
             action in
-            
-            let textFields:Array<UITextField>? =  alert.textFields as Array<UITextField>?
+            let textFields:Array<UITextField>? = alert.textFields as Array<UITextField>?
             
             if textFields != nil {
                 for textField:UITextField in textFields! {
-                    //各textにアクセス
-                    print(textField.text)
+                    switch editType {
+                    case .editMode:
+                        // テーブルの変更
+                        self.myItems[currentRow] = textField.text!;
+                        break
+                    case .addMode:
+                        // テーブルの追加
+                        self.myItems.insert(textField.text!, at: self.myItems.count - 1)
+                        break
+                    }
                 }
             }
             
-            
-            
-            // myItemsに追加.
-            self.myItems.add("add Cell")
-            // TableViewを再読み込み.
+            // TableViewを再読み込み
             self.personTableView.reloadData()
         })
         
+        // キャンセルボタン
         alert.addAction(UIAlertAction(title: "キャンセル", style: UIAlertActionStyle.cancel))
         alert.addAction(defaultAction)
+        
+        // テキストフィールドの設置
         alert.addTextField(configurationHandler: {(text:UITextField!) -> Void in
             text.placeholder = "入力してください"
-            
+            if (editType == .editMode) {
+                text.text = self.myItems[currentRow] as? String
+            }
             let label:UILabel = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
             label.text = "人物名"
             text.leftView = label
             text.leftViewMode = UITextFieldViewMode.always
-            
-            
         })
         
         present(alert, animated: true, completion: nil)
-        
-        
-        
     }
-    
-    
-    
 }
 
 
