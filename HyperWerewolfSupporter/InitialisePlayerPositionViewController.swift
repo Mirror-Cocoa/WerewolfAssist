200792e0d7321199095c3cc68c982be0a19d5986
Ichiro Miura <great_faily@icloud.com>
2018-08-22 20:15:57 +0900

    机のセット方式変更、バリデーションチェック

--- HyperWerewolfSupporter/InitialisePlayerPositionViewController.swift
+++ HyperWerewolfSupporter/InitialisePlayerPositionViewController.swift
@@ -16,20 +16,9 @@ class InitialisePlayerPositionViewController: UIViewController {
         super.viewDidLoad()
         
         // 戻るを消す
-        self.navigationItem.hidesBackButton = true
-        
-        
+//        self.navigationItem.hidesBackButton = true
         // アラートをセット
         initSetAlert()
-        
-        
-        
-        
-        
-        
-        
-        
-        
     }
     
     
@@ -47,27 +36,52 @@ class InitialisePlayerPositionViewController: UIViewController {
         })
         
         alert.addAction(cancelAction)
-        
+
         // OKボタンの処理
         let defaultAction: UIAlertAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler:{
             // ボタンが押された時の処理を書く（クロージャ実装）
             action in
             
+            var success = false
             // 値の受け取り
             let textFields:Array<UITextField>? = alert.textFields as Array<UITextField>?
             if (textFields != nil) {
                 for textField:UITextField in textFields! {
-                    // 初期人数
-                    self.personNum = Int(textField.text!)!
+                    if (self.isOnlyNumber(textField.text!)) {
+                        if (4 <= Int(textField.text!)! && Int(textField.text!)! <= 20) {
+                            // 初期人数
+                            self.personNum = Int(textField.text!)!
+                            success = true
+                        } else {
+                            // 注意文言アラート
+                            let warningAlert: UIAlertController = UIAlertController(title: "", message: "人数は4人〜20人の間で入力してください", preferredStyle:  UIAlertControllerStyle.alert)
+                            // キャンセルボタン
+                            let warningCancelAction: UIAlertAction = UIAlertAction(title: "はい", style: UIAlertActionStyle.default, handler:{
+                                action in
+                                self.navigationController!.popViewController(animated: true)
+                            })
+                            warningAlert.addAction(warningCancelAction)
+                            self.present(warningAlert, animated: true, completion: nil)
+                        }
+                    } else {
+                        // 注意文言アラート
+                        let warningAlert: UIAlertController = UIAlertController(title: "", message: "人数は数値で入力してください", preferredStyle:  UIAlertControllerStyle.alert)
+                        // キャンセルボタン
+                        let warningCancelAction: UIAlertAction = UIAlertAction(title: "キャンセル", style: UIAlertActionStyle.default, handler:{
+                            action in
+                            self.navigationController!.popViewController(animated: true)
+                        })
+                        
+                        warningAlert.addAction(warningCancelAction)
+                        self.present(warningAlert, animated: true, completion: nil)
+                        
+                    }
                 }
             }
-            // TODO:この画面に遷移する
-            // 大テーブルをセット
-            self.squareTablePositionSet()
-            
-            
-
-            
+            if (success){
+                // 大テーブルをセット
+                self.squareTablePositionSet()
+            }
         })
         
         alert.addAction(defaultAction)
@@ -76,21 +90,25 @@ class InitialisePlayerPositionViewController: UIViewController {
 //        alert.view.addSubview(createStepper())
         
         alert.addTextField(configurationHandler: {(text:UITextField!) -> Void in
-            text.placeholder = "人数を入力してください"
             text.text = "\(self.personNum)"
+            text.keyboardType = UIKeyboardType.numberPad
 
             let label:UILabel = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
             label.text = "人数"
             text.leftView = label
             text.leftViewMode = UITextFieldViewMode.always
+            
         })
-        
-        
-        
+
         present(alert, animated: true, completion: nil)
     }
     
-    
+    // 数字のみかを調べる。
+    func isOnlyNumber(_ str:String) -> Bool {
+        let predicate = NSPredicate(format: "SELF MATCHES '\\\\d+'")
+        return predicate.evaluate(with: str)
+    }
+
     /**
       * ステッパーの設置
       */
@@ -113,9 +131,6 @@ class InitialisePlayerPositionViewController: UIViewController {
 ////        createStepper().value = Double(personNum)
 //    }
     
-    
-    
-    
     /**
      * 机を描画する
      */
@@ -123,7 +138,7 @@ class InitialisePlayerPositionViewController: UIViewController {
         let outerTableRect = CGRect.init(x: CGFloat(10.0),
                                          y: (self.navigationController?.navigationBar.frame.size.height)! + 10,
                                          width: self.view.frame.width / 2 - 10,
-                                         height: self.view.frame.height / 2
+                                         height: self.view.frame.width / 2 - 10
         )
         let outerTable = UIView.init(frame: outerTableRect)
         outerTable.backgroundColor = UIColor.gray
@@ -142,39 +157,37 @@ class InitialisePlayerPositionViewController: UIViewController {
         
         var innerTableList: Array<CGRect> = Array(repeating: CGRect.zero, count: self.personNum)
         
-        switch self.personNum {
-        case 4:
-            // 上下左右。下が自分。
-            innerTableList = innerTablePisitioning(positionList: [15,7,2,10], outerRect: outerRect)
-            break
-        case 5:
-            // 斜め全部と下。下が自分。
-            innerTableList = innerTablePisitioning(positionList: [15,7,2,10], outerRect: outerRect)
-            break
-        default:
-            break
-        }
+        innerTableList = innerTablePisitioning(positionCount: self.personNum, outerRect: outerRect)
         
         var cnt = 0
         for innerTableRect in innerTableList {
             let innerTable = UIView.init(frame: innerTableRect)
-            switch cnt{
-            case 0:
-                innerTable.backgroundColor = UIColor.blue
-                break
-            case 1:
-                innerTable.backgroundColor = UIColor.red
-                break
-            case 2:
-                innerTable.backgroundColor = UIColor.yellow
-                break
-            case 3:
-                innerTable.backgroundColor = UIColor.green
-                break
-            default:
-                break
-            }
+            innerTable.backgroundColor = UIColor.init(red: 230/255, green: 255/255, blue: 230/255, alpha: 90/100)
+            
+//            switch cnt{
+//            case 0:
+//                innerTable.backgroundColor = UIColor.blue
+//                break
+//            case 1:
+//                innerTable.backgroundColor = UIColor.red
+//                break
+//            case 2:
+//                innerTable.backgroundColor = UIColor.yellow
+//                break
+//            case 3:
+//                innerTable.backgroundColor = UIColor.green
+//                break
+//            default:
+//                break
+//            }
             self.view.addSubview(innerTable)
+            
+            let label: UILabel = UILabel(frame:CGRect(x:0,y:0,width:innerTableRect.width,height:innerTableRect.height))
+            label.text = String(cnt)
+            label.textColor = UIColor.black
+            label.textAlignment = NSTextAlignment.center
+            
+            innerTable.addSubview(label)
             
             cnt = cnt + 1
         }
@@ -184,67 +197,84 @@ class InitialisePlayerPositionViewController: UIViewController {
     /**
      * 内部テーブルのポジショニング指定
      */
-    func innerTablePisitioning(positionList: [Int], outerRect: CGRect) -> [CGRect] {
+    func innerTablePisitioning(positionCount: Int, outerRect: CGRect) -> [CGRect] {
         let innerTableSize = CGSize(width: outerRect.width / 7, height: outerRect.height / 6)
         var innerTableList: Array<CGRect> = Array(repeating: CGRect.zero, count: self.personNum)
         
-        /*
-            1  2  3  4  5  6  7
-            8                12
-            9                13
-           10                14
-           11                15
-           16 17 18 19 20 21 22
-         
-        */
+        let wholeDistance = outerRect.width * 2 + outerRect.height * 2 - innerTableSize.width * 2 - innerTableSize.height * 2
+        let moveDistance = (wholeDistance / CGFloat(positionCount))
         
-        var innerCount = 0
-        for position in positionList {
-            var xPosition = outerRect.minX
-            var yPosition = outerRect.minY
-            let minusCoefficient = CGFloat(position % 4)
+        let rightX = outerRect.maxX - innerTableSize.width
+        let underY = outerRect.maxY - innerTableSize.height
+        
+        let initXPosition = outerRect.maxX - ((outerRect.width + innerTableSize.width) / 2)
+        let initYPosition = underY
+        
+        var xPosition = initXPosition
+        var yPosition = initYPosition
+        
+        for pos in 0..<positionCount {
+            if (pos == 0) {
+                innerTableList[pos] = CGRect.init(
+                    x: xPosition, y: yPosition, width: innerTableSize.width, height: innerTableSize.height
+                )
+                continue
+            }
+        
+            // Y軸が下にいたら
+            if (yPosition == underY) {
+                // xPositionが最低限を超えていたら
+                if (xPosition - moveDistance < outerRect.minX) {
+                    yPosition -= abs(moveDistance - xPosition)
+                    xPosition = outerRect.minX
+                } else {
+                    xPosition -= moveDistance
+                }
             
-            switch position {
-            case (1...4):
-                xPosition += innerTableSize.width * minusCoefficient
-                break
-
-            case (5...8):
-                yPosition += innerTableSize.height * minusCoefficient
-                break
-
-            case (9...12):
-                xPosition += outerRect.width - innerTableSize.width
-                yPosition += innerTableSize.height * minusCoefficient
-                break
-
-            case (13...16):
-                xPosition += innerTableSize.width * minusCoefficient
-                yPosition += outerRect.height - innerTableSize.height
-
-                break
-                
-            default:
-                break
+            }
+            // X軸が左にいたら
+            else if (xPosition == outerRect.minX) {
+                // yPositionが最低限を超えていたら
+                if (yPosition - moveDistance < outerRect.minY) {
+                    xPosition += abs(moveDistance - yPosition)
+                    yPosition = outerRect.minY
+                } else {
+                    yPosition -= moveDistance
+                }
             }
             
-            innerTableList[innerCount] = CGRect.init(
-                    x: xPosition, y: yPosition, width: innerTableSize.width, height: innerTableSize.height
+            // Y軸が上にいたら
+            else if (yPosition == outerRect.minY) {
+                // xPositionが最上限を超えていたら
+                if (xPosition + moveDistance > rightX) {
+                    yPosition += moveDistance - (rightX - xPosition)
+                    xPosition = rightX
+                } else {
+                    xPosition += moveDistance
+                }
+            }
+            
+            // X軸が右にいたら
+            else if (xPosition == rightX) {
+                // yPositionが最上限を超えていたら
+                if (yPosition + moveDistance > underY) {
+                    xPosition -= moveDistance - (underY - yPosition)
+                    yPosition = underY
+                } else {
+                    yPosition += moveDistance
+                }
+            }
+        
+            innerTableList[pos] = CGRect.init(
+                x: xPosition, y: yPosition, width: innerTableSize.width, height: innerTableSize.height
             )
-            innerCount = innerCount + 1
         }
         
         return innerTableList
     }
     
-    
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
 }
-
-
