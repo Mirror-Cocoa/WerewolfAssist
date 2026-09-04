5b3205aca6aab966d8c147612682101a9c21e660
Ichiro Miura <great_faily@icloud.com>
2026-09-04 14:42:13 +0900

    現行XcodeでビルドできるようCocoaPods設定を修正

--- HyperWerewolfSupporter/ViewController.swift
+++ HyperWerewolfSupporter/ViewController.swift
@@ -8,36 +8,36 @@
 
 import Foundation
 import UIKit
-import Gecco
+//import Gecco
 
-class viewController: UIViewController, SpotlightViewControllerDelegate {
-    
-    private var spotlightViewController: SpotlightViewController!
-    
-    var stepIndex: Int = 0
-    
-    @IBOutlet var img: UIImageView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-//        self.img.image = UIImage(named:"IMG_12.png")
-        // 初期化
-        self.spotlightViewController = SpotlightViewController()
-        self.spotlightViewController?.delegate = self
-    }
-    
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-                navigationController?.setNavigationBarHidden(true, animated: false)
-        
-        
-        present(self.spotlightViewController, animated: true) {
-            let w = self.view.frame.width
-            let h = self.view.frame.height
-            
-            self.spotlightViewController.spotlightView.appear(Spotlight.Oval(center: CGPoint(x: w / 2 + 105, y: h - 17.5), diameter: 50))
-        }
-    }
-    
-}
+//class viewController: UIViewController, SpotlightViewControllerDelegate {
+//    
+//    private var spotlightViewController: SpotlightViewController!
+//    
+//    var stepIndex: Int = 0
+//    
+//    @IBOutlet var img: UIImageView!
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+////        self.img.image = UIImage(named:"IMG_12.png")
+//        // 初期化
+//        self.spotlightViewController = SpotlightViewController()
+//        self.spotlightViewController?.delegate = self
+//    }
+//    
+//    
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//                navigationController?.setNavigationBarHidden(true, animated: false)
+//        
+//        
+//        present(self.spotlightViewController, animated: true) {
+//            let w = self.view.frame.width
+//            let h = self.view.frame.height
+//            
+//            self.spotlightViewController.spotlightView.appear(Spotlight.Oval(center: CGPoint(x: w / 2 + 105, y: h - 17.5), diameter: 50))
+//        }
+//    }
+//    
+//}
--- Podfile
+++ Podfile
@@ -1,10 +1,15 @@
-# Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
+platform :ios, '12.0'
 
 target 'HyperWerewolfSupporter' do
-  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
 
-  # Pods for HyperWerewolfSupporter
   pod 'Gecco'
+end
+
+post_install do |installer|
+  installer.pods_project.targets.each do |target|
+    target.build_configurations.each do |config|
+      config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '12.0'
+    end
+  end
 end
